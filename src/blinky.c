5aef64d0d6c9e5536547815cf213cab66de1993a
qlinkPro <kulunk52@gmail.com>
2026-03-30 00:20:05 +0300

    unnecessary files deleted.

--- src/blinky.c
+++ src/blinky.c
@@ -1,64 +1,15 @@
-//*****************************************************************************
-//
-// blinky.c - Simple example to blink the on-board LED.
-//
-// Copyright (c) 2013-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 2.1.4.178 of the EK-TM4C1294XL Firmware Package.
-//
-//*****************************************************************************
-
 #include <stdint.h>
 #include <stdbool.h>
 #include "hw_memmap.h"
-#include "debug.h"
 #include "gpio.h"
 #include "sysctl.h"
-
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>Blinky (blinky)</h1>
-//!
-//! A very simple example that blinks the on-board LED using direct register
-//! access.
-//
-//*****************************************************************************
-
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
-#ifdef DEBUG
-void
-__error__(char *pcFilename, uint32_t ui32Line)
-{
-    while(1);
-}
-#endif
 
 //*****************************************************************************
 //
 // Blink the on-board LED.
 //
 //*****************************************************************************
-int
-main(void)
+int main(void)
 {
     volatile uint32_t ui32Loop;
 
@@ -70,9 +21,7 @@ main(void)
     //
     // Check if the peripheral access is enabled.
     //
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
-    {
-    }
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION));
 
     //
     // Enable the GPIO pin for the LED (PN0).  Set the direction as output, and
